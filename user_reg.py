434cbd5773b88767994e65323739c13173f951b6
CoderButNoob <sonaraniket20@gmail.com>
2025-04-07 20:53:01 +0530

    Use Case 3 Valid Email

--- user_reg.py
+++ user_reg.py
@@ -11,11 +11,21 @@ def valid_last_name(last_name):
         return True
     else:
         return "Inavlid : Name Should be min 3 chars "
+    
+def valid_email(email):
+    email_pattern = r"^[\w]+(\.[\w]+)?@[\w]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,}])?$"
+
+    if re.match(email_pattern,email):
+        return  "Valid Email"
+    else:
+        return "Invalid Email"
 
 
-first_name = input("Enter Fisrst Name")
-last_name = input("Enter Last Name")
+first_name = input("Enter Fisrst Name : ")
+last_name = input("Enter Last Name : ")
+email = input("Enter Email : ")
 
 
 print(valid_last_name(last_name))
 print(valid_name (first_name))
+print(valid_email(email))
