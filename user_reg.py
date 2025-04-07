10ff27f3e414bac488f11a484a0943287fca6404
CoderButNoob <sonaraniket20@gmail.com>
2025-04-07 20:37:05 +0530

    Use Case 2 Valid Last Name

--- user_reg.py
+++ user_reg.py
@@ -6,5 +6,16 @@ def valid_name(name):
     else:
         return "Inavlid : Name Should be min 3 chars "
 
-name = input()
-print(valid_name (name))
+def valid_last_name(last_name):
+    if re.match(r"^[A-Z][a-z]{2,}$",last_name):
+        return True
+    else:
+        return "Inavlid : Name Should be min 3 chars "
+
+
+first_name = input("Enter Fisrst Name")
+last_name = input("Enter Last Name")
+
+
+print(valid_last_name(last_name))
+print(valid_name (first_name))
