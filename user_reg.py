e8740a8b5d4a08886bc852dc2054e666b51b47f6
CoderButNoob <sonaraniket20@gmail.com>
2025-04-08 15:17:41 +0530

    Use case 5 valid password Rule 1: Min 8 chars

--- user_reg.py
+++ user_reg.py
@@ -27,14 +27,25 @@ def valid_phone(phone):
     else:
         return "Number is Invalid pls check"
 
+def valid_password(password):
+    # Rule 1 :- Min 8 chars
+    password_pattern = r"^[\w]{8}$"
+    if re.match(password_pattern,password):
+        return "Valid Password"
+    else:
+        return "Invalid Password"
+
 
 first_name = input("Enter Fisrst Name : ")
 last_name = input("Enter Last Name : ")
 email = input("Enter Email : ")
 phone = input("ENter phone number in format +91 1234567891 : ")
+password = input("Enter Password")
+
 
 
 print(valid_last_name(last_name))
 print(valid_name (first_name))
 print(valid_email(email))
 print(valid_phone(phone))
+print(valid_password(password))
