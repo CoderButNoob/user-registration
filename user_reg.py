5fc4dbb5ce5d993dbf98fc7b7f320c10c1562b44
CoderButNoob <sonaraniket20@gmail.com>
2025-04-08 14:12:16 +0530

    Use case 4 valid Phone

--- user_reg.py
+++ user_reg.py
@@ -20,12 +20,21 @@ def valid_email(email):
     else:
         return "Invalid Email"
 
+def valid_phone(phone):
+    phone_pattern = r"^(\+91)\s[0-9]{10}$"
+    if re.match(phone_pattern,phone):
+        return "Valid Number"
+    else:
+        return "Number is Invalid pls check"
+
 
 first_name = input("Enter Fisrst Name : ")
 last_name = input("Enter Last Name : ")
 email = input("Enter Email : ")
+phone = input("ENter phone number in format +91 1234567891 : ")
 
 
 print(valid_last_name(last_name))
 print(valid_name (first_name))
 print(valid_email(email))
+print(valid_phone(phone))
